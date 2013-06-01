c651fed776bad269782f03481d120ba5a4dace8f
Jeremy Stewart <jlstewart379@gmail.com>
2013-06-01 17:10:47 -0400

    Testing getting root element by id

--- lib/furter/accessors/screen.rb
+++ lib/furter/accessors/screen.rb
@@ -2,13 +2,16 @@ module Furter
   module Accessors
     class Screen
 
-
       def big_dump
         send_get('dump')
       end
 
       def get_window(element)
         big_dump[element]
+      end
+
+      def get_view_by_id(id)
+        big_dump
       end
 
     end
--- spec/lib/furter/accessors/screen_spec.rb
+++ spec/lib/furter/accessors/screen_spec.rb
@@ -5,41 +5,76 @@ describe Furter::Accessors::Screen do
   let(:screen) { Furter::Accessors::Screen.new}
   let(:server){double('frank-server')}
 
+  let(:json) {
+    MultiJson.load(
+    '{
+      "frame":
+      {
+        "size":
+        {
+          "width":768,
+          "height":1024
+        },
+        "origin":
+        {
+          "x":0,
+          "y":0
+        }
+      },
+        "uid":1,
+        "backgroundColor":{},
+        "isHidden":0,
+        "subviews":["a_subview"],
+        "tag":0,
+        "isKeyWindow":1,
+        "accessibilityFrame":{},
+        "windowLevel":0,
+        "alpha":1,
+        "autoresizingMask":0,
+        "class":"UIWindow"
+    }')
+   }
 
   context 'Getting screen data' do
-
     it 'can get all the screen data' do
       screen.should_receive(:send_get).with 'dump'
       screen.big_dump
     end
-
-    context 'getting root elements' do
-
-      it 'can get window subview' do
-        json = MultiJson.load('{"class":"UIWindow","subviews":["a_subview"],"uid":1}')
-        screen.should_receive(:send_get).with('dump').and_return(json)
-        screen.get_window("subviews").should == ["a_subview"]
-      end
-      #
-      #it 'can get the window id' do
-      #  json = MultiJson.load('{"class":"UIWindow","subviews":["a_subview"],"uid":1}')
-      #  screen.should_receive(:send_get).with('dump').and_return(json)
-      #  screen.
-      #
-      #end
-
-    end
-
-
   end
 
+  context 'getting root elements' do
 
+    before(:each){
+      screen.should_receive(:send_get).with('dump').and_return(json)
+    }
 
+    it 'can get all the screen elements' do
+      screen.should_receive(:send_get).with('dump').and_return(json)
+      screen.big_dump
+    end
 
+    it 'can get window subview' do
+      screen.get_window("subviews").should == ["a_subview"]
+    end
 
+    it 'can get the window id' do
+      screen.get_window("uid").should == 1
+    end
 
+    it 'can get the window element class' do
+      screen.get_window("class").should == "UIWindow"
+    end
 
+    context 'getting the frame properties' do
+      it 'can get the frame properties' do
+        screen.get_window("frame").should == {"size"=>{"width"=>768, "height"=>1024}, "origin"=>{"x"=>0, "y"=>0}}
+      end
+    end
 
-
-
+    context 'getting screen elements by id' do
+      it 'can get a view by id' do
+        screen.get_view(1).should == json
+      end
+    end
+  end
 end
